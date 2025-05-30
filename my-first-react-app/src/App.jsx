daba4af51be9fdf58027c7fc6217d747a2ea5e66
Pedro Medeiros <pereira.medeiros2@academico.ifrn.edu.br>
2025-05-30 09:47:28 -0300

    debounced is set

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import {useDebounce} from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
@@ -21,13 +22,18 @@ const App = ()=>{
   const [errorMessage, setErrorMessage] = useState('')
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-  const fetchMovies = async () => {
+  useDebounce( ()=> setDebouncedSearchTerm(searchTerm), 500, 
+  [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
   setIsLoading(true);
   setErrorMessage('');
 
-  const endpoint = searchTerm
-    ? `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(searchTerm)}`
+
+  const endpoint = query
+    ? `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`
     : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc`;
 
   try {
@@ -49,8 +55,8 @@ const App = ()=>{
 };
 
   useEffect(()=> {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return(
     <>
